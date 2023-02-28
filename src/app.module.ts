a4b04f99f48ab0ebf4e35ae9c99a02b2bdbee9a6
duong0712 <doduong7122000@gmail.com>
2023-02-28 14:01:28 +0700

    'commit'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [ 
+    MongooseModule.forRoot('mongodb+srv://doduong:04060064@cluster0.8pcps1b.mongodb.net/student?retryWrites=true&w=majority'),
+    StudentModule ],
   controllers: [AppController],
   providers: [AppService],
 })
